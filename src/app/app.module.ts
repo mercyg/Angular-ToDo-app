2871fd10bee2b6e9fa6254ec8980fffc93b4300c
Meheret Egzerab <meheretgirma22@gmail.com>
2018-07-11 22:21:53 -0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddTodoComponent } from './add-todo/add-todo.component';
+import { ListTodoComponent } from './list-todo/list-todo.component';
+import { MatInputModule, MatFormFieldModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddTodoComponent,
+    ListTodoComponent
   ],
   imports: [
-    BrowserModule
+    MatFormFieldModule,
+    MatInputModule,
+    BrowserModule,
+    BrowserAnimationsModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
